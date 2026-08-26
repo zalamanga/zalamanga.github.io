68b8fcf7d15d410bbff42d14fb4e405a2a649b53
zalamanga <rafikmas78@gmail.com>
2026-08-26 15:28:02 +0700

    Add employee attendance project

--- assets/js/partials.js
+++ assets/js/partials.js
@@ -1,6 +1,6 @@
 async function loadPartials() {
   const includes = [...document.querySelectorAll('[data-include]')];
-  const version = '20260826-portfolio';
+  const version = '20260826-attendance';
 
   for (const slot of includes) {
     const url = slot.dataset.include;
